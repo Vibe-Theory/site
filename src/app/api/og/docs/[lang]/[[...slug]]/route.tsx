af368263fde0593275cfda215858db9e33d9740f
Baran Mordoğan <baran@okunamayanad.com>
2026-01-03 04:41:31 +0300

    feat: replace the placeholder design of opengraph images

--- src/app/api/og/docs/[lang]/[[...slug]]/route.tsx
+++ src/app/api/og/docs/[lang]/[[...slug]]/route.tsx
@@ -1,12 +1,10 @@
+/* eslint-disable @next/next/no-img-element */
 import { ImageResponse } from "next/og";
 import { source } from "@/lib/source";
 import { NextRequest } from "next/server";
 import { notFound } from "next/navigation";
-
-const size = {
-  width: 1200,
-  height: 630,
-};
+import { readFile } from "fs/promises";
+import { join } from "path";
 
 export async function GET(
   _request: NextRequest,
@@ -19,22 +17,128 @@ export async function GET(
     return notFound();
   }
 
+  const promises = [];
+
+  // prettier-ignore
+  {
+    // promises.push(readFile(join(process.cwd(), "assets/geist/Geist-Thin.otf")));
+    // promises.push(readFile(join(process.cwd(), "assets/geist/Geist-ThinItalic.otf")));
+    // promises.push(readFile(join(process.cwd(), "assets/geist/Geist-ExtraLight.otf")));
+    // promises.push(readFile(join(process.cwd(), "assets/geist/Geist-ExtraLightItalic.otf")));
+    // promises.push(readFile(join(process.cwd(), "assets/geist/Geist-Light.otf")));
+    // promises.push(readFile(join(process.cwd(), "assets/geist/Geist-LightItalic.otf")));
+    promises.push(readFile(join(process.cwd(), "assets/geist/Geist-Regular.otf")));
+    promises.push(readFile(join(process.cwd(), "assets/geist/Geist-RegularItalic.otf")));
+    // promises.push(readFile(join(process.cwd(), "assets/geist/Geist-Medium.otf")));
+    // promises.push(readFile(join(process.cwd(), "assets/geist/Geist-MediumItalic.otf")));
+    promises.push(readFile(join(process.cwd(), "assets/geist/Geist-SemiBold.otf")));
+    promises.push(readFile(join(process.cwd(), "assets/geist/Geist-SemiBoldItalic.otf")));
+    // promises.push(readFile(join(process.cwd(), "assets/geist/Geist-Bold.otf")));
+    // promises.push(readFile(join(process.cwd(), "assets/geist/Geist-BoldItalic.otf")));
+    // promises.push(readFile(join(process.cwd(), "assets/geist/Geist-ExtraBold.otf")));
+    // promises.push(readFile(join(process.cwd(), "assets/geist/Geist-ExtraBoldItalic.otf")));
+    // promises.push(readFile(join(process.cwd(), "assets/geist/Geist-Black.otf")));
+    // promises.push(readFile(join(process.cwd(), "assets/geist/Geist-BlackItalic.otf")));
+  }
+
+  const [
+    // Thin,
+    // ThinItalic,
+    // ExtraLight,
+    // ExtraLightItalic,
+    // Light,
+    // LightItalic,
+    Regular,
+    RegularItalic,
+    // Medium,
+    // MediumItalic,
+    SemiBold,
+    SemiBoldItalic,
+    // Bold,
+    // BoldItalic,
+    // ExtraBold,
+    // ExtraBoldItalic,
+    // Black,
+    // BlackItalic,
+  ] = await Promise.all(promises);
+
   return new ImageResponse(
     <div
       style={{
-        fontSize: 128,
-        background: "white",
-        width: "100%",
+        backgroundColor: "#0a0a0a",
+        color: "white",
         height: "100%",
+        width: "100%",
         display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        padding: "96px",
       }}
     >
-      {page.data.title}
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "16px",
+        }}
+      >
+        <span
+          style={{
+            fontSize: 64,
+            fontWeight: 600,
+          }}
+        >
+          {page.data.title}
+        </span>
+        <span
+          style={{
+            fontSize: 40,
+            fontWeight: 400,
+            color: "lightgray",
+          }}
+        >
+          {page.data.description}
+        </span>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          gap: "24px",
+          alignItems: "center",
+          fontSize: 48,
+          fontWeight: 600,
+        }}
+      >
+        <img
+          src={`${process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"}/branding/logo-light-348.png`}
+          alt="Logo"
+          width={64}
+          height={64}
+        />
+        <span>Hytale Modding</span>
+      </div>
     </div>,
     {
-      ...size,
+      fonts: [
+        // { name: "Geist", data: Thin, weight: 100, style: "normal" },
+        // { name: "Geist", data: ThinItalic, weight: 100, style: "italic" },
+        // { name: "Geist", data: ExtraLight, weight: 200, style: "normal" },
+        // { name: "Geist", data: ExtraLightItalic, weight: 200, style: "italic" },
+        // { name: "Geist", data: Light, weight: 300, style: "normal" },
+        // { name: "Geist", data: LightItalic, weight: 300, style: "italic" },
+        { name: "Geist", data: Regular, weight: 400, style: "normal" },
+        { name: "Geist", data: RegularItalic, weight: 400, style: "italic" },
+        // { name: "Geist", data: Medium, weight: 500, style: "normal" },
+        // { name: "Geist", data: MediumItalic, weight: 500, style: "italic" },
+        { name: "Geist", data: SemiBold, weight: 600, style: "normal" },
+        { name: "Geist", data: SemiBoldItalic, weight: 600, style: "italic" },
+        // { name: "Geist", data: Bold, weight: 700, style: "normal" },
+        // { name: "Geist", data: BoldItalic, weight: 700, style: "italic" },
+        // { name: "Geist", data: ExtraBold, weight: 800, style: "normal" },
+        // { name: "Geist", data: ExtraBoldItalic, weight: 800, style: "italic" },
+        // { name: "Geist", data: Black, weight: 900, style: "normal" },
+        // { name: "Geist", data: BlackItalic, weight: 900, style: "italic" },
+      ],
     },
   );
 }
